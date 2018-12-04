734688e2f321405ef57653da9187c02c8b0fe306
tduffy000 <tduffy000@citymail.cuny.edu>
2018-12-04 09:41:25 -0500

    update Comparable in attribute.py

--- attribute.py
+++ attribute.py
@@ -5,8 +5,17 @@ class Attribute:
         self.less_than_value = None
         self.more_than_value = None
 
+    def __lt__(self, other):
+        return self.name < other.name
+
+    def __gt__(self, other):
+        return self.name > other.name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
     def set_less_than_value(self, value):
          self.less_than_value = value
-    
+
     def set_more_than_value(self, value):
          self.more_than_value = value
